cbbab73a7ea8ed7523aeabd35e1debecf3aeaffe
Kolbynko <90548034+Kolbynko@users.noreply.github.com>
2021-10-13 10:00:16 +0200

    Update Bubble sort.py

--- Bubble sort.py
+++ Bubble sort.py
@@ -8,9 +8,7 @@ def bubble_sort(val):
         change=False
         for i in range(len(val)-1):
             if val[i]>val[i+1]:
-                x=val[i]
-                val[i]=val[i+1]
-                val[i+1]=x
+                x,val[i]=val[i],x
                 change=True
     return val
 print(bubble_sort(cisla))
